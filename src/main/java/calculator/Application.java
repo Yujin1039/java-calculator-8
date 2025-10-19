6e1c094bb2ce43488016128bdb7b80117419ce59
Yujin1039 <shelly103989@gmail.com>
2025-10-19 21:40:30 +0900

    feat: 계산하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,20 +6,32 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
+        System.out.println("결과 : "+sum(str));
     }
 
-    private static String[] getSepAndExp(String str){
+    private static int sum(String str){
+        int total = 0;
+
+        String[] calStr = getSeparator(str);
+        String[] numArr = calStr[1].split("[,:"+calStr[0]+"]");
+
+        for(String s: numArr){
+            total += Integer.parseInt(s);
+        }
+
+        return total;
+    }
+
+    private static String[] getSeparator(String str){
         String[] tempArr = str.split("//|\\\\n");
         String addedSep = "";
         String trimmedStr = tempArr[0];
 
-        // 커스텀 구분자가 존재하는 경우
         if(tempArr.length == 3 && !tempArr[1].isEmpty()){
             addedSep = tempArr[1];
             trimmedStr = tempArr[2];
         }
 
-        // 기타 구분자 존재 여부 확인
         String[] chkSepArr = trimmedStr.split("["+addedSep+",:0-9]");
         for (String s : chkSepArr) {
             if (!s.isEmpty()) {
@@ -27,7 +39,6 @@ public class Application {
             }
         }
 
-        // 커스텀 구분자 & 구분자가 포함된 계산식 반환
         String[] trimmedArr = {addedSep, trimmedStr};
         return trimmedArr;
     }
