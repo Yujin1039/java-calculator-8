b0a26995c18717bae1957f56a3f2e2a9df57a730
Yujin1039 <shelly103989@gmail.com>
2025-10-19 21:38:33 +0900

    feat: 커스텀 구분자 추출 및 예외 처리

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -7,4 +7,28 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String str = Console.readLine();
     }
+
+    private static String[] getSepAndExp(String str){
+        String[] tempArr = str.split("//|\\\\n");
+        String addedSep = "";
+        String trimmedStr = tempArr[0];
+
+        // 커스텀 구분자가 존재하는 경우
+        if(tempArr.length == 3 && !tempArr[1].isEmpty()){
+            addedSep = tempArr[1];
+            trimmedStr = tempArr[2];
+        }
+
+        // 기타 구분자 존재 여부 확인
+        String[] chkSepArr = trimmedStr.split("["+addedSep+",:0-9]");
+        for (String s : chkSepArr) {
+            if (!s.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        // 커스텀 구분자 & 구분자가 포함된 계산식 반환
+        String[] trimmedArr = {addedSep, trimmedStr};
+        return trimmedArr;
+    }
 }
